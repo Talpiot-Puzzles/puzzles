553b4a26f49ca2c263806dec1cfae0fe3a34f2b7
DeveloopClick <63965693+DeveloopClick@users.noreply.github.com>
2023-05-02 19:25:14 +0300

    Update pipeline.py
    added error messages, nicer make_pipleline

--- pipeline.py
+++ pipeline.py
@@ -1,88 +1,98 @@
 class Pipeline:
-    def __init__(self, steps, input=None):
+    def __init__(self, steps, pipeline_input=None):
         self.accessible_data = None
-        self.input = input
+        self.pipeline_input = pipeline_input
         self.steps = steps
 
     def run(self):
         # Run the pipeline steps in sequence
-        data = input
-        for step in self.steps:
-            data = step[1](data)
+        data = pipeline_input
+        for step_name, step_func in self.steps:
+            try:
+                data = step_func(data)
+            except Exception as e:
+                print(f"Error running step '{step_name}': {str(e)}")
+                break
         return data
-    
-    
+
+
 # Step 1: Load images from database
 def load_images(image_dir):
     image_paths = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith(".jpg")]
     images = [cv2.imread(path) for path in image_paths]
     return images
 
+
 # Step 2: Preprocess images
 def preprocess_images(images):
-
     preprocessed_images = [preprocess_image(image) for image in images]
     return preprocessed_images
 
+
 # Step 3: Anchor detection
 def detect_anchors(preprocessed_images):
-
     anchors = detect_anchors_from_images(preprocessed_images)
     return anchors
 
+
 # Step 4: Connect images
 def connect_images(anchors):
-
     connected_images = connect_images_from_anchors(anchors)
     return connected_images
 
+
 # Step 5: Shift images
 def shift_images(connected_images):
-
     shifted_images = focus_by_lower_plane(connected_images)
     return shifted_images
 
+
 # Step 6: Combine images
 def combine_images(shifted_images):
-
     combined_image = smart_combine_images(shifted_images)
     return combined_image
 
+
 # Step 7: Object detection
 def detect_objects(combined_image):
-    def detect_objects_in_image(image):
-        # TODO: implement object detection
-        labeled_image = None
-        return labeled_image
-
     labeled_image = detect_objects_in_image(combined_image)
     return labeled_image
 
-# Define the updated pipeline
-full_pipeline = [
-    ('load_images', FunctionTransformer(load_images)),
-    ('preprocess_images', FunctionTransformer(preprocess_images)),
-    ('detect_anchors', FunctionTransformer(detect_anchors)),
-    ('connect_images', FunctionTransformer(connect_images)),
-    ('shift_images', FunctionTransformer(shift_images)),
-    ('combine_images', FunctionTransformer(combine_images)),
-    ('detect_objects', FunctionTransformer(detect_objects)),
-]
 
-def partial_pipeline(start_step=None, end_step=None):
+def make_pipeline(start_step=None, end_step=None, pipeline_input=None):
+    # Define the full pipeline
+    full_pipeline = [
+        ('load_images', FunctionTransformer(load_images)),
+        ('preprocess_images', FunctionTransformer(preprocess_images)),
+        ('detect_anchors', FunctionTransformer(detect_anchors)),
+        ('connect_images', FunctionTransformer(connect_images)),
+        ('shift_images', FunctionTransformer(shift_images)),
+        ('combine_images', FunctionTransformer(combine_images)),
+        ('detect_objects', FunctionTransformer(detect_objects)),
+    ]
+
+    # Get the start and end indices of the pipeline
     if start_step is not None:
-        start_index = [i for i, (name, _) in enumerate(pipeline) if name == start_step][0]
+        try:
+            start_index = [i for i, (name, _) in enumerate(full_pipeline) if name == start_step][0]
+        except IndexError:
+            raise ValueError(f"Invalid start step: {start_step}")
     else:
         start_index = 0
-        
+
     if end_step is not None:
-        end_index = [i for i, (name, _) in enumerate(pipeline) if name == end_step][0] + 1
+        try:
+            end_index = [i for i, (name, _) in enumerate(full_pipeline) if name == end_step][0] + 1
+        except IndexError:
+            raise ValueError(f"Invalid end step: {end_step}")
     else:
-        end_index = len(pipeline)
-        
-    return Pipeline(pipeline[start_index:end_index])
+        end_index = len(full_pipeline)
+
+    steps = full_pipeline[start_index:end_index]
+    return Pipeline(steps, pipeline_input)
+
 
 # Example usage
-p = partial_pipeline(start_step='load_images', end_step='combine_images')
-p.input = '/path/to/image/directory'
-combined_image = p.run()
+input_data = '/path/to/image/directory'
+p = make_pipeline(start_step='load_images', end_step='combine_images', pipeline_input=input_data)
+output_data = p.run()
